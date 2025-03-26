0b5decef12f6eb0ab8eaf895aa1657f4fb275438
Burak Can Körpınar <burikcankorpinar7@gmail.com>
2025-03-26 22:24:41 +0300

    ss

--- emlak/next.config.js
+++ emlak/next.config.js
@@ -10,18 +10,6 @@ const nextConfig = {
       },
     ],
   },
-  typescript: {
-    // !! UYARI !!
-    // Bu seçenek build sırasında TypeScript hatalarını görmezden gelir
-    // Sadece geçici bir çözüm olarak kullanılmalıdır
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    // !! UYARI !!
-    // Bu seçenek build sırasında ESLint hatalarını görmezden gelir
-    // Sadece geçici bir çözüm olarak kullanılmalıdır
-    ignoreDuringBuilds: true,
-  },
 };
 
 module.exports = nextConfig;
